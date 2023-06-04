79eb00245c606897844e221b56d395fa3218b5c0
Jason <jyshowalter@gmail.com>
2023-06-04 09:39:42 -0700

    finished lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector ('form')
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    taskList (document.getElementById ('new-task-description').value)
+    form.reset ()
+  })
 });
+
+function taskList (task) {
+  let p = document.createElement ('p')
+  let btn = document.createElement ('button')
+  btn.addEventListener ('click', handleDelete)
+  btn.textContent = ' x '
+  p.textContent = `${task} `
+  p.appendChild (btn)
+  document.querySelector ('#list').appendChild(p)
+}
+
+function handleDelete (e) {
+  e.target.parentNode.remove ()
+}
